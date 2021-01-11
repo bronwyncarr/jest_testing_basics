007590848cef44342ec178aaec5f0c79e92eabda
bronwyncarr <bronwynacarr@gmail.com>
2021-01-11 21:01:16 +1100

    sample tests for a calculator

---
+++ sample.test.js
@@ -0,0 +1,19 @@
+describe("Calculator", () => {
+  describe(".add()", () => {
+    it("adds number b and number a", () => {});
+
+    it.todo("throws and error if not passed numbers");
+
+    it("handles negative numbers", () => {});
+  });
+
+  describe(".subtract()", () => {
+    it("subtracts number b from number a", () => {
+      expect(true).toBe(false);
+    });
+
+    it("throws and error if not passed numbers", () => {});
+
+    it.skip("handles negative numbers", () => {});
+  });
+});
